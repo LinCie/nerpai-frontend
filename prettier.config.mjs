83900e064f6567d1a29312daa77fd75ad4690aa4
LinCie <aldiofernanda@protonmail.com>
2025-03-27 16:09:16 +0700

    initial commit

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 2,
+  semi: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
