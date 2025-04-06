4f710aba37a54e9d062ae30aae92566d557feb47
LinCie <aldiofernanda@protonmail.com>
2025-04-06 16:19:48 +0700

    fix: fix hydration error

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
@@ -7,23 +8,30 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 function Header() {
   const { setTheme, theme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
 
   return (
     <header className="border-sidebar-accent bg-sidebar sticky flex items-center justify-between border-b px-4 py-3">
       <h1 className="text-foreground text-lg font-semibold">Game Bodo</h1>
 
       <div className="flex items-center gap-4">
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          {theme === "dark" ? (
-            <Sun className="h-5 w-5 text-yellow-300" />
-          ) : (
-            <Moon className="text-foreground h-5 w-5" />
-          )}
-        </Button>
+        {mounted && (
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {theme === "dark" ? (
+              <Sun className="h-5 w-5 text-yellow-300" />
+            ) : (
+              <Moon className="text-foreground h-5 w-5" />
+            )}
+          </Button>
+        )}
 
         <Avatar>
           <AvatarImage src="/avatar.png" alt="@user" />
